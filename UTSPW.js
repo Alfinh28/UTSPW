efa1aa600429ec8198219705bf9eb467210cbcec
Alfinh28 <144680250+Alfinh28@users.noreply.github.com>
2023-11-19 18:10:08 +0700

    Add files via upload

---
+++ UTSPW.js
@@ -0,0 +1,8 @@
+//pemberitahuan 
+//prompt('berapa usia anda?')
+alert('SELAMAT DATANG DI KONVEKSI EKO')
+
+const menuToggle = document.querySelector('.menu-toggle input') ;
+const nav = document.querySelector('nav ul');
+
+
